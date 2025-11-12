189f8487e90a8d76063fabbfcd31253ad625af08
Lise TUONG <lise.tuong@ipsa.fr>
2025-11-12 14:02:50 +0100

    ajout de bordures

--- test pyside6 layouts.py
+++ test pyside6 layouts.py
@@ -8,6 +8,7 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QWidget,
+    QFrame
 )
 
 class MainWindow(QMainWindow):
@@ -22,14 +23,31 @@ class MainWindow(QMainWindow):
         layout3 = QVBoxLayout()
         layout4 = QVBoxLayout()
 
-        layout2.addWidget(QLabel("Stats"))
-        layout2.addWidget(QLabel("Avions"))
-        layout3.addWidget(QLabel("Vue de haut"))
-        layout3.addWidget(QLabel("Message"))
-        layout3.addWidget(QLabel("Piste de côté"))
-        layout4.addWidget(QLabel("Contrôles"))
-        layout4.addWidget(QLabel("Instructions"))
-        layout4.addWidget(QLabel("Actions"))
+        label = QLabel("Stats")
+        label.setFrameShape(QFrame.Panel)
+        label2 = (QLabel("Avions"))
+        label2.setFrameShape(QFrame.Panel)
+        label3 = (QLabel("Vue de haut"))
+        label3.setFrameShape(QFrame.Panel)
+        label4 = (QLabel("Message"))
+        label4.setFrameShape(QFrame.Panel)
+        label5 = (QLabel("Piste de côté"))
+        label5.setFrameShape(QFrame.Panel)
+        label6 = (QLabel("Contrôles"))
+        label6.setFrameShape(QFrame.Panel)
+        label7 = (QLabel("Instructions"))
+        label7.setFrameShape(QFrame.Panel)
+        label8 = (QLabel("Actions"))
+        label8.setFrameShape(QFrame.Panel)
+
+        layout2.addWidget(label)
+        layout2.addWidget(label2)
+        layout3.addWidget(label3)
+        layout3.addWidget(label4)
+        layout3.addWidget(label5)
+        layout4.addWidget(label6)
+        layout4.addWidget(label7)
+        layout4.addWidget(label8)
 
         layout1.addLayout(layout2)
         layout1.addLayout(layout3)
