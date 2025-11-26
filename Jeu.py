0d765083945310ea452a7fae5722827be173fd5e
Lise TUONG <lise.tuong@ipsa.fr>
2025-11-26 10:27:31 +0100

    réorganisation du code

--- Jeu.py
+++ Jeu.py
@@ -290,20 +290,6 @@ class MainGameWindow(QMainWindow):
         self.bar_vitesse.setFormat("Vitesse : %v km/h")
         self.bar_fuel.setFormat("Carburant : %v %")
 
-        layout_urgence_attente = QHBoxLayout()
-        layout_urgence_attente.setSpacing(5)
-        layout_urgence_attente.addWidget(btn_urgence)
-        layout_urgence_attente.addWidget(btn_attente)
-
-        layout_controles.addWidget(self.bar_altitude)
-        layout_controles.addWidget(self.bar_vitesse)
-        layout_controles.addWidget(self.bar_fuel)
-
-        layout_controles.addLayout(layout_urgence_attente)
-        layout_controles.addWidget(btn_atterrir)
-
-        group_controles.setLayout(layout_controles)
-
         # Valeurs initiales
         self.bar_altitude.setMaximum(12000)
         self.bar_altitude.setValue(0)
@@ -318,6 +304,20 @@ class MainGameWindow(QMainWindow):
         self.bar_fuel.setValue(0)
         self.update_progress_bar_spectrum(self.bar_fuel, 0, 100, [(255,0,0),(255,255,0)])
 
+        # Layouts
+        layout_urgence_attente = QHBoxLayout()
+        layout_urgence_attente.setSpacing(5)
+        layout_urgence_attente.addWidget(btn_urgence)
+        layout_urgence_attente.addWidget(btn_attente)
+
+        layout_controles.addWidget(self.bar_altitude)
+        layout_controles.addWidget(self.bar_vitesse)
+        layout_controles.addWidget(self.bar_fuel)
+
+        layout_controles.addLayout(layout_urgence_attente)
+        layout_controles.addWidget(btn_atterrir)
+
+        group_controles.setLayout(layout_controles)
 
         group_instructions = QGroupBox("Instructions")
         layout_instructions = QVBoxLayout()
